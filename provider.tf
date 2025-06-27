ef0e0d8e9a8c36e65932341dc0f0bc319e88c44a
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-27 17:12:01 +0200

    27/06/2025 - Updated configuration and fixed bugs

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
-    }
     podman = {
       source = "project0/podman"
     }
@@ -11,6 +7,6 @@ terraform {
 }
 
 provider "podman" {
-  alias        = "pod"
-  socket_path  = "unix:///var/folders/yx/5vkcx21x6h99nz2r0r3glcpw0000gn/T/podman/podman-machine-default-api.sock"
+  alias = "pod"
+  uri   = "unix:///var/folders/yx/5vkcx21x6h99nz2r0r3glcpw0000gn/T/podman/podman-machine-default-api.sock"
 }
